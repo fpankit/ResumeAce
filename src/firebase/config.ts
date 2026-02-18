1f95c9aa2b4d7e813b5b6bbfa0fa67cfef4d48da
Admin Zepmeds <zepmedsa@gmail.com>
2026-02-18 14:26:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5662393501-55931",
+  "appId": "1:850248280854:web:9b9d5a98f290b25a503e37",
+  "apiKey": "AIzaSyDDcnmNb7BI94fhUdHt9G060EHExdtOPyc",
+  "authDomain": "studio-5662393501-55931.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "850248280854"
+};
